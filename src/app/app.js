2790a396324c8270e1b1421ed46592dba4cffb76
tongyuan.zou <tongyuan.zou@shuyun.com>
2014-10-14 19:22:47 +0800

    update

--- src/app/app.js
+++ src/app/app.js
@@ -21,6 +21,9 @@ angular.module('App', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngRes
                 templateUrl: 'app/main/main.html',
                 controller: 'MainCtrl'
             })
+            .when('/dom', {
+                templateUrl: 'app/dom/dom1.html'
+            })
             .otherwise({
                 redirectTo: '/'
             });
