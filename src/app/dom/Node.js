89b5cc147cfb790b91742ea655488d1dcbbcf322
starzou <starzou@126.com>
2014-10-18 19:51:59 +0800

    add Node

---
+++ src/app/dom/Node.js
@@ -0,0 +1,17 @@
+/*
+ * https://github.com/starzou
+ *
+ * Copyright (c) 2014 StarZou
+ * Licensed under the MIT license.
+ */
+
+/**
+ *@class Node
+ *@description Node 类型讲解
+ *@time 2014-10-18 19:47
+ *@author StarZou
+ **/
+'use strict';
+
+var title = document.getElementById('title');
+console.log(title);
