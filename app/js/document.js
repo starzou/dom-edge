a29d9c89e51e1ef6c0553220d08259f51e75a0fe
starzou <starzou@126.com>
2014-10-19 21:52:08 +0800

    添加 document.createDocumentFragment 案例

--- app/js/document.js
+++ app/js/document.js
@@ -20,3 +20,16 @@ console.log(document);
 console.log(tags);
 console.log(tags.namedItem('feel'));
 console.log(classes);
+
+var newsFragment = document.createDocumentFragment(); // 创建一个文档片段，实现高效DOM操作
+var ul = document.getElementById('newsList'),
+    news = ['贪官扮买菜老汉出逃', '新郎8台挖掘机迎亲', '朝鲜欲建天空敢死队'],
+    li;
+
+for (var i = 0; i < news.length; i++) {
+    li = document.createElement('li');
+    li.appendChild(document.createTextNode(news[i]));
+    newsFragment.appendChild(li);
+}
+ul.appendChild(newsFragment);
+
