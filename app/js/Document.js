2e4d89391257648e0d32d148843a454e1b1f16f0
starzou <starzou@126.com>
2014-10-19 13:29:13 +0800

    完成 HTMLDocument 讲解

--- app/js/Document.js
+++ app/js/Document.js
@@ -13,4 +13,10 @@
  **/
 'use strict';
 
+var tags = document.getElementsByTagName('*'),
+    classes = document.getElementsByClassName('red');
+
 console.log(document);
+console.log(tags);
+console.log(tags.namedItem('feel'));
+console.log(classes);
