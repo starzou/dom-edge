f78bd4ac6f6c5ab0389d9377d282aef5446cc22e
starzou <starzou@126.com>
2014-10-19 13:45:28 +0800

    add Element

---
+++ app/js/element.js
@@ -0,0 +1,16 @@
+/*
+ * https://github.com/starzou
+ *
+ * Copyright (c) 2014 StarZou
+ * Licensed under the MIT license.
+ */
+
+/**
+ *@class element
+ *@description
+ *@time 2014-10-19 13:41
+ *@author StarZou
+ **/
+'use strict';
+
+console.log('loaded...');
