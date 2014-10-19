2440331e57aca8a851c9cab40be350d4df9b5e7f
starzou <starzou@126.com>
2014-10-19 18:30:34 +0800

    完成 Element

--- app/js/element.js
+++ app/js/element.js
@@ -15,5 +15,23 @@
 
 console.log('loaded...');
 
+// 属性的操作
 var divElement = document.getElementById('div');
-console.log(divElement);
+console.log(divElement.getAttribute('my-self'));
+
+divElement.setAttribute('my-data', Date.now());
+divElement.removeAttribute('id');
+
+
+// 通过对象.getAttribute() 与 对象.属性 访问访问属性不同
+var sureButton = document.getElementById('btnSure');
+console.log(sureButton.onclick);
+console.log(sureButton.getAttribute('onclick'));
+
+console.log(sureButton.style);
+console.log(sureButton.getAttribute('style'));
+
+sureButton.setAttribute('my-data', '123');
+sureButton.setAttribute('onclick', 'console.log(Date.now());');
+sureButton.onclick();
+console.log(sureButton.getAttribute('onclick'));
