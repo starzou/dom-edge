95501a5a3ceb23128abcf66d838687edeebd167b
starzou <starzou@126.com>
2014-11-02 15:42:38 +0800

    DOM 扩展

--- app/js/dom-extend.js
+++ app/js/dom-extend.js
@@ -13,7 +13,10 @@
  **/
 'use strict';
 
-
+/**
+ * querySelector(); 示例
+ * @type {HTMLElement}
+ */
 var div = document.querySelector('#main-div'),
     h1 = div.querySelector('.red'),
     p = div.querySelector('.green');
@@ -24,13 +27,46 @@ var queryElement = document.querySelector('#main-div>.green');
 console.log(queryElement);
 console.log(queryElement === p);
 
-
+/**
+ * classList
+ */
 var button = div.querySelector('button');
 button.onclick = function () {
     h1.classList.toggle('green');
+//    addElement();
 };
+
+/**
+ * querySelectorAll(); 示例
+ * @type {NodeList}
+ */
+var divs = document.querySelectorAll('div');
+console.log(divs);
 
 // dataset
 var html = document.documentElement;
 html.dataset.version = 'version_' + Date.now();
 console.log(html.innerHTML);
+
+
+/**
+ * innerHTML
+ * @type {HTMLElement}
+ */
+var innerDiv = document.querySelector('#innerDiv'), str = '';
+function addElement() {
+    for (var i = 1; i < 60; i++) {
+        str += '<li>' + i + '</li>';
+    }
+    innerDiv.innerHTML = str;
+    console.log(innerDiv.innerText);
+}
+
+/**
+ * scrollIntoView();
+ * @type {HTMLElement}
+ */
+var showBtn = document.getElementById('showBtn');
+showBtn.onclick = function () {
+    button.scrollIntoView();
+};
