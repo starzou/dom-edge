3500efc861c5fbe34235df9771b84cc91991e8e1
starzou <starzou@126.com>
2014-11-02 17:38:40 +0800

    DOM 2 & 3

---
+++ app/js/dom3.js
@@ -0,0 +1,14 @@
+/*
+ * https://github.com/starzou
+ *
+ * Copyright (c) 2014 StarZou
+ * Licensed under the MIT license.
+ */
+
+/**
+ *@class dom3
+ *@description
+ *@time 2014-11-02 17:36
+ *@author StarZou
+ **/
+'use strict';
