55a26118a850cd293fb3e7af18626ba748d54caf
starzou <starzou@126.com>
2014-11-02 22:51:52 +0800

    DOM 2 & 3

--- app/js/dom3.js
+++ app/js/dom3.js
@@ -12,3 +12,51 @@
  *@author StarZou
  **/
 'use strict';
+
+var h1 = document.getElementById('title');
+console.log(h1.style.getPropertyCSSValue('color'));
+console.log(h1.style.getPropertyValue('color'));
+
+var disableButton = document.getElementById('disableButton'),
+    insertRuleButton = document.getElementById('insertRuleButton'),
+    iterButton = document.getElementById('iterButton'),
+    div = document.getElementById('div'),
+    styleSheets = document.styleSheets,
+    styleSheet;
+
+/**
+ * 禁用样式
+ */
+disableButton.onclick = function () {
+    for (var i = 0; i < styleSheets.length; i++) {
+        styleSheet = styleSheets[i]
+        if (styleSheet.disabled) {
+            styleSheet.disabled = false;
+            disableButton.innerHTML = '禁用';
+        } else {
+            styleSheet.disabled = true;
+            disableButton.innerHTML = '开启';
+        }
+    }
+};
+
+/**
+ *插入样式规则
+ */
+insertRuleButton.onclick = function () {
+    styleSheets[0].insertRule('#title{width: 300px;border: 5px solid green;}', 0);
+//    styleSheets[1].deleteRule(0);
+};
+
+/**
+ * 遍历元素
+ */
+iterButton.onclick = function () {
+    var iter = document.createNodeIterator(div, NodeFilter.SHOW_ELEMENT, null, false);
+    var node;
+    while (node = iter.nextNode()) {
+        console.log(node);
+    }
+};
+
+
